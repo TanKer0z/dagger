2a18b9d9d0658ef1e224781b8ebf77df06e6cb2e
Atlante <108891261+AtlanteFr@users.noreply.github.com>
2023-08-10 08:32:34 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,72 +1,53 @@
 local S = minetest.get_translator("dagger")
 
-local function calculate_dagger_damage(distance)
-    if distance <= 1 then
-        return 50 -- critical damage
-    elseif distance <= 2 then
-        return 15
-    elseif distance <= 3 then
-        return 7
-    elseif distance <= 4 then
-        return 4
-    elseif distance <= 5 then
-        return 2
+minetest.register_tool("dagger:dagger", {
+    description = "Dagger",
+    inventory_image = "dagger.png",
+    tool_capabilities = {
+		full_punch_interval = 1.0,
+        max_drop_level = 0,
+        groupcaps = {
+            snappy = {times = {[2] = 2.0}, uses = 25, maxlevel = 2},
+        },
+        damage_groups = {fleshy = 1},
+    },
+})
+
+local function calculate_damage(distance)
+    if distance == 0 then
+        return math.random(35, 50)
+    elseif distance == 1 then
+        return math.random(15, 34)
+    elseif distance == 2 then
+        return math.random(1, 5)
+    elseif distance == 3 then
+        return math.random(1, 3)
+    elseif distance == 4 then
+        return math.random(1, 2)
     else
-        return 1
+        return 0
     end
 end
 
-minetest.register_tool("dagger:dagger", {
-    description = S("Dagger\nThe closer you are to your target the more damage you will do."),
-    inventory_image = "dagger.png",
-    tool_capabilities = {
-        full_punch_interval = 0.5,
-        max_drop_level = 1,
-        groupcaps = {
-            snappy = {times = {[2] = 1.0}, uses = 25, maxlevel = 2},
-        },
-        damage_groups = {fleshy = 12},
-    },
-    on_use = function(itemstack, user, pointed_thing)
-            if user and user:is_player() then
-                local user_pos = user:get_pos()
+minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage)
+    local wielded_item = hitter:get_wielded_item()
+    if wielded_item:get_name() == "dagger:dagger" then
+        local hitter_pos = hitter:get_pos()
+        local player_pos = player:get_pos()
+        local distance = vector.distance(hitter_pos, player_pos)
 
-                if pointed_thing and pointed_thing.type == "object" then
-                    local target = pointed_thing.ref
-                    local target_pos = target:get_pos()
-                    local distance = vector.distance(user_pos, target_pos)
+        local calculated_damage = calculate_damage(math.floor(distance))
 
-                    if distance <= 4 then
-                        local damage = calculate_dagger_damage(distance)
-                        target:punch(user, nil, {damage_groups = {fleshy = damage}})
+        if calculated_damage > 0 then
+            local player_hp = player:get_hp()
+            player:set_hp(player_hp - calculated_damage)
 
-                        local particle_pos = vector.add(target_pos, {x = 0, y = 1, z = 0})
-                        minetest.add_particlespawner({
-                            amount = 10,
-                            time = 0.5,
-                            minpos = particle_pos,
-                            maxpos = particle_pos,
-                            minvel = {x = -1, y = 0, z = -1},
-                            maxvel = {x = 1, y = 2, z = 1},
-                            minacc = {x = 0, y = -5, z = 0},
-                            maxacc = {x = 0, y = -9, z = 0},
-                            minexptime = 0.5,
-                            maxexptime = 1.5,
-                            minsize = 3,
-                            maxsize = 4,
-                            texture = "blood.png",
-                        })
-                    if itemstack:get_wear() + 1 <= 65535 then
-                    itemstack:add_wear(65535 / 25)
-                    end
-                end
-            end
-        else
-            return ItemStack("")
+            local wear_increase = 65535 / 25
+            wielded_item:add_wear(wear_increase)
+            hitter:set_wielded_item(wielded_item)
         end
-        return itemstack
-    end,
-})
+    end
+end)
 
 minetest.register_craft({
     output = "dagger:dagger",
@@ -76,4 +57,3 @@ minetest.register_craft({
         {"", "default:stick", ""}
     }
 })
-
